2c9b64301619b48ad3b6f9f1bceabfed6a1eb8f3
Ran Chen <ranchen27056@gmail.com>
2020-03-19 17:53:19 -0400

    create user table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_19_004706) do
+ActiveRecord::Schema.define(version: 2020_03_19_213830) do
 
   create_table "articles", force: :cascade do |t|
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string "title"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
