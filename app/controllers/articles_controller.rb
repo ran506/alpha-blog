952a077fa07cd8678e8a47653d12316ec4a5e709
Ran Chen <ranchen27056@gmail.com>
2020-03-19 00:01:15 -0400

    add partcials and destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController <ApplicationController
+    def index
+        @articles = Article.all
+    end
+        
     def new
         @article = Article.new
     end
@@ -15,6 +19,26 @@ class ArticlesController <ApplicationController
             render 'new'
         end
     end
+    def edit
+        @article = Article.find(params[:id])
+    end
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)  
+          flash[:notice] = "Article was successfully updated"
+           redirect_to article_path(@article)
+        else
+          render 'edit'
+        end
+        end
+    
+    def destroy
+        @article =Article.find(params[:id])
+        @article.destroy
+        flash[:notice]="Article was successfully deleted"
+        redirect_to article_path
+    end
+    
     def show
         @article = Article.find(params[:id])
     end 
