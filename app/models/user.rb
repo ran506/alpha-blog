a52f19796381f309be0affe8aba17a130ed268d0
Ran Chen <ranchen27056@gmail.com>
2020-03-19 23:42:01 -0400

    create profile image and show user's articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
     uniqueness: { case_sensitive: false },
     format: { with: VALID_EMAIL_REGEX }
 
+    
     validates :user_id, presence: true
-
     has_secure_password
 end
