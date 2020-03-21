91dfa048a9cc849bfdbfafcc1d9b3d13bc351cc0
Ran Chen <ranchen27056@gmail.com>
2020-03-21 11:21:47 -0400

    add categories model and test

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'will_paginate', '3.2.1'
 gem 'will_paginate-bootstrap4'
 gem 'bcrypt', '~> 3.1.7'
 gem 'sqlite3', '~> 1.4'
+gem 'rails-controller-testing'
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
 # Use SCSS for stylesheets
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_20_003659) do
+ActiveRecord::Schema.define(version: 2020_03_21_024328) do
 
   create_table "articles", force: :cascade do |t|
     t.text "description"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 2020_03_20_003659) do
     t.integer "user_id"
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
@@ -27,6 +33,7 @@ ActiveRecord::Schema.define(version: 2020_03_20_003659) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "passward_digest"
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
